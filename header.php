9f243e100ef641269092952d584bddf42e676433
Andy Chapman <achapman@degdigital.com>
2018-04-20 22:51:01 -0400

    Header Began

--- header.php
+++ header.php
@@ -48,8 +48,10 @@
 
 				<div id="inner-header" class="wrap cf">
 
+					<img src="<?php echo get_template_directory_uri(); ?>/library/images/crest.png" class="logo" alt="<?php bloginfo('name'); ?>"/>
+
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<h1 itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
