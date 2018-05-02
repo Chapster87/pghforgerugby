d60fcc0c788984d2ad21a1d3cd260939b64fa46d
Andy Chapman <achapman@degdigital.com>
2018-05-02 09:15:54 -0400

    Submenus Started

--- header.php
+++ header.php
@@ -69,11 +69,11 @@
     					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
     					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
     					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
+    					         'before' => '<div class="submenu-toggle"></div>',                                 // before the menu
+        			             'after' => '',                                  // after the menu
+        			             'link_before' => '',                            // before each link
+        			             'link_after' => '',                             // after each link
+        			             'depth' => 0,                                   // limit the depth of the nav
     					         'fallback_cb' => ''                             // fallback function (if there is one)
 						)); ?>
 
