93a465780c5b94bc068034afa1cf58c2e8e909bd
Andy Chapman <achapman@degdigital.com>
2018-05-01 22:39:50 -0400

    Footer Social Media

--- footer.php
+++ footer.php
@@ -20,6 +20,12 @@
 
 					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
 
+					<ul class="social-links">
+						<li><a href="https://www.facebook.com/pittsburghrugby" target="_blank" ><span class="visually-hidden">Facebook</span><i class="fab fa-facebook"></i></a></li>
+						<li><a href="https://www.instagram.com/pittsburghrugby/" target="_blank" ><span class="visually-hidden">Instagram</span><i class="fab fa-instagram"></i></a></li>
+						<li><a href="https://twitter.com/pittsburghrugby" target="_blank" ><span class="visually-hidden">Twitter</span><i class="fab fa-twitter"></i></a></li>
+					</ul>
+
 				</div>
 
 			</footer>
