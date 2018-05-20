b74603e25025f40c924946b0b1bbb0f7e571a613
Andy Chapman <achapman@degdigital.com>
2018-05-20 18:42:48 -0400

    Tracking More Files

---
+++ build/header.php
@@ -0,0 +1,86 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+
+		<?php // mobile meta (hooray!) ?>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<?php // or, set /favicon.ico for IE10 win ?>
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<?php // wordpress head functions ?>
+		<?php wp_head(); ?>
+		<?php // end of wordpress head ?>
+
+		<?php // drop Google Analytics Here ?>
+		<?php // end analytics ?>
+		<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/library/js/slick/slick-theme.css"/>
+
+	</head>
+
+	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+
+		<div id="container">
+
+			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+
+				<div id="inner-header" class="wrap">
+
+					<div class="logo-block">
+						<h1>
+							<a href="<?php echo home_url(); ?>" rel="nofollow" class="logo-link">
+								<img src="<?php echo get_template_directory_uri(); ?>/library/images/pghforgerugby.png" class="logo" alt="<?php bloginfo('name'); ?>"/>
+								<span class="header-title" itemscope itemtype="http://schema.org/Organization">Pittsburgh Forge <span class="title-rugbyclub">Rugby Club</span></span>
+							</a>
+						</h1>
+
+						<?php // if you'd like to use the site description you can un-comment it below ?>
+						<?php // bloginfo('description'); ?>
+					</div>
+
+
+					<nav class="main-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+						<span class="mm-btn mobile-tablet-only"><i class="fas fa-bars"></i></span>
+						<?php wp_nav_menu(array(
+    					         'container' => false,                           // remove nav container
+    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
+    					         'theme_location' => 'main-nav',                 // where it's located in the theme
+    					         'before' => '<div class="submenu-toggle"><i class="fas fa-caret-right"></i></div>',                                 // before the menu
+        			             'after' => '',                                  // after the menu
+        			             'link_before' => '',                            // before each link
+        			             'link_after' => '',                             // after each link
+        			             'depth' => 0,                                   // limit the depth of the nav
+    					         'fallback_cb' => ''                             // fallback function (if there is one)
+						)); ?>
+
+					</nav>
+
+				</div>
+
+			</header>
