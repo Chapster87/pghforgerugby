d0ff203c84d93bdde8d546d29085635741928d24
Andy Chapman <achapman@degdigital.com>
2018-12-06 14:59:49 -0500

    Desktop style complete

--- build/footer.php
+++ build/footer.php
@@ -1,8 +1,8 @@
 <div class="sponsors__bar">
 	<div class="sponsors__bar-inner wrap cf">
-		<a href="https://pghrugby.com/dugan-associates-supports-pittsburghs-rugby-club" class="sponsor dugan"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/dugan.png" alt="Dugan & Associates" class="sponsor__logo"></a>
-		<a href="https://pghrugby.com/pittsburgh-forge-partners-with-essmc" class="sponsor essmc"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/essmc.png" alt="Eastern Suburbs Sports Medicine" class="sponsor__logo"></a>
-		<a href="https://pghrugby.com/mhk-supports-the-pittsburgh-forge" class="sponsor mhk"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/mhk.png" alt="Malec, Herring & Krause" class="sponsor__logo"></a>
+		<a href="/dugan-associates-supports-pittsburghs-rugby-club" class="sponsor dugan"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/dugan.png" alt="Dugan & Associates" class="sponsor__logo"></a>
+		<a href="/pittsburgh-forge-partners-with-essmc" class="sponsor essmc"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/essmc.png" alt="Eastern Suburbs Sports Medicine" class="sponsor__logo"></a>
+		<a href="/mhk-supports-the-pittsburgh-forge" class="sponsor mhk"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/mhk.png" alt="Malec, Herring & Krause" class="sponsor__logo"></a>
 		<a href="http://ruggerspub.com/" class="sponsor ruggers" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/sponsors/ruggers.png" alt="Ruggers Pub" class="sponsor__logo"></a>
 	</div>
 </div>
@@ -12,34 +12,41 @@
 	<div id="inner-footer" class="wrap cf">
 
 		<div class="footer__row row-1">
-			<div class="forge-blast">
-				<img src="<?php echo get_template_directory_uri(); ?>/library/images/forge_blast_drop.png" class="blast-logo" alt="<?php bloginfo('name'); ?>"/>
+			<div class="footer__row--left">
+				<div class="join-forge">
+					<a href="/contact" class="join-forge__link">
+						<img src="<?php echo get_template_directory_uri(); ?>/library/images/forge_blast_drop.png" class="blast-logo" alt="<?php bloginfo('name'); ?>"/>
+						<h3 class="join-forge__headline">Ready to get into the action?!<br/> Click here to conact us!</h3>
+					</a>
+				</div>
 			</div>
 
-			<div class="social">
-				<h3>Join us on social Media:</h3>
-				<ul class="social__links">
-					<li><a href="https://www.facebook.com/pittsburghrugby" target="_blank"><span class="visually-hidden">Facebook</span><i class="fab fa-facebook"></i></a></li>
-					<li><a href="https://www.instagram.com/pittsburghrugby/" target="_blank"><span class="visually-hidden">Instagram</span><i class="fab fa-instagram"></i></a></li>
-					<li><a href="https://twitter.com/pittsburghrugby" target="_blank"><span class="visually-hidden">Twitter</span><i class="fab fa-twitter"></i></a></li>
-				</ul>
-			</div>
+			<div class="footer__row--right">
+				<div class="social">
+					<h3 class="social__header">Find us on Social Media:</h3>
+					<ul class="social__links">
+						<li><a href="https://www.facebook.com/pittsburghrugby" target="_blank"><span class="visually-hidden">Facebook</span><i class="fab fa-facebook"></i></a></li>
+						<li><a href="https://www.instagram.com/pittsburghrugby/" target="_blank"><span class="visually-hidden">Instagram</span><i class="fab fa-instagram"></i></a></li>
+						<li><a href="https://twitter.com/pittsburghrugby" target="_blank"><span class="visually-hidden">Twitter</span><i class="fab fa-twitter"></i></a></li>
+					</ul>
+				</div>
 
-			<nav role="navigation">
-				<?php wp_nav_menu(array(
-					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
-					'theme_location' => 'footer-links',             // where it's located in the theme
-					'before' => '',                                 // before the menu
-					'after' => '',                                  // after the menu
-					'link_before' => '',                            // before each link
-					'link_after' => '',                             // after each link
-					'depth' => 0,                                   // limit the depth of the nav
-					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-					)); ?>
-			</nav>
+				<nav role="navigation">
+					<?php wp_nav_menu(array(
+						'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+						'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+						'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+						'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+						'theme_location' => 'footer-links',             // where it's located in the theme
+						'before' => '',                                 // before the menu
+						'after' => '',                                  // after the menu
+						'link_before' => '',                            // before each link
+						'link_after' => '',                             // after each link
+						'depth' => 0,                                   // limit the depth of the nav
+						'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+						)); ?>
+				</nav>
+			</div>
 		</div>
 
 		<div class="footer__row row-2">
