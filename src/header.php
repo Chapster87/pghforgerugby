bf751a0db0518af2d500daad7a136944217d7b60
Andy Chapman <achapman@degdigital.com>
2019-07-14 21:01:07 -0400

    Remove GTM for now

--- src/header.php
+++ src/header.php
@@ -49,11 +49,11 @@
 		<?php // end of wordpress head ?>
 
 		<?php // Google Tag Manager ?>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-5S7GHD2');</script>
+		})(window,document,'script','dataLayer','GTM-5S7GHD2');</script> -->
 		<?php // End Google Tag Manager ?>
 
 		<?php // drop Google Analytics Here ?>
@@ -63,8 +63,8 @@
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5S7GHD2"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5S7GHD2"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
 		<!-- End Google Tag Manager (noscript) -->
 
 		<div id="container">
