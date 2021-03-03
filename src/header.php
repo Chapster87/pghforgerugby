1bb353636622499b451d9165e494e513dca74a23
Andy Chapman <achapman@degdigital.com>
2021-03-03 10:24:46 -0500

    add symptom checker

--- src/header.php
+++ src/header.php
@@ -112,6 +112,6 @@
 
 			<div class="wrap">
 				<div class="headline-links in-header">
-					<div class="notice-link"><h3>Check out our <a href="/covid-19-rtp-plan">Return to Play Safety Plan</a> or <a href="https://docs.google.com/forms/d/e/1FAIpQLSfBmsIIINm8mthgQtKACouU1cnivvNtLpRTG8cTem85yVQPpg/viewform?embedded=true" class="fancybox-iframe">Sign the Participation Waiver</a>.</h3></div>
+				<div class="notice-link"><h3>Check out our <a href="/covid-19-rtp-plan">Return to Play Safety Plan</a>, <a href="https://docs.google.com/forms/d/e/1FAIpQLSfBmsIIINm8mthgQtKACouU1cnivvNtLpRTG8cTem85yVQPpg/viewform?embedded=true" class="fancybox-iframe">Sign the Participation Waiver</a>, or <a href="https://docs.google.com/forms/d/e/1FAIpQLSeYenqyxnPkFdAYqKXNT85vFWRIqsLblkfs1QcOr_5rq9riqw/viewform?embedded=true" class="fancybox-iframe">Fill out the Symptom Checker</a>.</h3></div>
 				</div>
 			</div>
