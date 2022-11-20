9b04f6ec11d894c65b69a8605170c2216f3e9bfe
Andy Chapman <achapman@degdigital.com>
2022-11-20 18:31:56 -0500

    PFRC-6-Form Styles, Start Product Form Controls Styling

--- templates/styleguide/forms.php
+++ templates/styleguide/forms.php
@@ -1,65 +1,71 @@
-<div class="form-group">
-	<label class="form-control-label" for="test">Text Field</label>
+<div class="form-group mb-3">
+	<label class="form-label" for="test">Text Field</label>
 	<input type="text" id="test" class="form-control" name="test"/>
 </div>
-<div class="form-group required">
-	<label class="form-control-label" for="test">Required Text Field</label>
+<div class="form-group mb-3 required">
+	<label class="form-label" for="test">Required Text Field</label>
 	<input type="text" id="test" class="form-control" name="test"/>
 </div>
-<div class="form-group">
-	<label class="form-control-label" for="test">Text Field with Error</label>
+<div class="form-group mb-3">
+	<label class="form-label" for="test">Text Field with Error</label>
 	<input type="text" id="test" class="form-control is-invalid" name="test"/>
 	<div class="invalid-feedback">This is a test error</div>
 </div>
-<div class="form-group">
-	<label class="form-control-label" for="test">Text Field with Help Text</label>
-	<input type="text" id="test" class="form-control" name="test"/>
-	<span>Example Help Text</span>
+<div class="form-group mb-3">
+	<label class="form-label" for="test">Text Field with Help Text</label>
+	<input type="text" id="test" class="form-control" name="test" aria-describedby="fieldHelp"/>
+	<div id="fieldHelp" class="form-text">Example Help Text</div>
 </div>
-<div class="form-group">
-	<label class="form-control-label" for="test">Text Field with Help Tooltip </label>
+<div class="form-group mb-3">
+	<label class="form-label" for="test">Text Field with Help Tooltip </label>
 	<span class="info-icon">
 		<span class="icon">i</span>
 		<div class="tooltip">Testing Tooltip Content</div>
 	</span>
 	<input type="text" id="test" class="form-control" name="test"/>
 </div>
-<div class="form-group">
-	<label class="form-control-label" for="test2">Select Box</label>
+<div class="form-group mb-3">
+	<label class="form-label" for="test2">Select Box</label>
 	<span class="info-icon">
 		<span class="icon">i</span>
 		<div class="tooltip">Testing Tooltip Content</div>
 	</span>
-	<select class="form-control custom-select" id="test2" name="test">
+	<select class="form-select form-control" id="test2" name="test" aria-label="test">
 		<option label=""></option>
 		<option label="Test Value 1">Test Value 1</option>
 		<option label="Test Value 2">Test Value 2</option>
-		<option label="Test Value 3">Test Value 3</option>  
+		<option label="Test Value 3">Test Value 3</option>
 	</select>
 </div>
 
-<div class="form-group custom-control custom-checkbox">
-	<input type="checkbox" class="custom-control-input" id="checkbox-test" name="checkbox-test" value="true"/>
-	<label class="custom-control-label" for="checkbox-test">Checkbox</label>
+<div class="form-group mb-3">
+	<div class="form-check">
+		<input type="checkbox" class="form-check-input" id="checkbox-test" name="checkbox-test" value="true" checked/>
+		<label class="form-check-label" for="checkbox-test">Checkbox</label>
+	</div>
+	<div class="form-check">
+		<input type="checkbox" class="form-check-input" id="checkbox-test2" name="checkbox-test2" value="true" disabled/>
+		<label class="form-check-label" for="checkbox-test2">Disabled</label>
+	</div>
 </div>
 
-<div class="form-group custom-control custom-radio">
-	<div class="start-lines">
-		<input id="radio-test1" name="radio-test" type="radio" class="custom-control-input" checked>
-		<label for="radio-test1" class="custom-control-label">
+<div class="form-group mb-3">
+	<div class="form-check">
+		<input type="radio" class="form-check-input" id="radio-test1"  name="radio-test" checked>
+		<label for="radio-test1" class="form-check-label">
 			<span>Option 1</span>
 		</label>
 	</div>
-	<div class="start-lines">
-		<input id="radio-test2" name="radio-test" type="radio" class="custom-control-input">
-		<label for="radio-test2" class="custom-control-label">
+	<div class="form-check">
+		<input type="radio" class="form-check-input" id="radio-test2" name="radio-test">
+		<label for="radio-test2" class="form-check-label">
 			<span>Option 2</span>
 		</label>
 	</div>
-	<div class="start-lines">
-		<input id="radio-test3" name="radio-test" type="radio" class="custom-control-input">
-		<label for="radio-test3" class="custom-control-label">
-			<span>Option 3</span>
+	<div class="form-check">
+		<input type="radio" class="form-check-input" id="radio-test3" name="radio-test" disabled>
+		<label for="radio-test3" class="form-check-label">
+			<span>Disabled</span>
 		</label>
 	</div>
 </div>
